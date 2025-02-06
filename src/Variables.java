2ff46b88e166fd95e7023afe2b54619fe6f9a8c1
dianazavr1 <2235.10002@manas.edu.kg>
2025-02-07 00:37:43 +0600

    Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int number = 10;
+        double price = 99.99;
+        String name = "Программист";
+
+        System.out.println("Число: " + number);
+        System.out.println("Цена: " + price);
+        System.out.println("Имя: " + name);
+    }
+}
