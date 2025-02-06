0237314d378d4ada21ae7b2965c257d0cf586b0c
dianazavr1 <2235.10002@manas.edu.kg>
2025-02-07 01:28:41 +0600

    WhileLoops.java

---
+++ src/WhileLoops.java
@@ -0,0 +1,11 @@
+public class WhileLoops {
+    public static void main (String[] args){
+        int a = 0;
+        while(a < 100){
+            if (a % 5 == 0){
+                System.out.println(a);
+            }
+            a++;
+        }
+    }
+}
