60b41742ad1da4cb59785a3a3a5ada854ef7ff14
dianazavr1 <2235.10002@manas.edu.kg>
2025-02-19 11:16:34 +0600

    BestTimeToBuyAndSellStock

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; // Обновляем минимальную цену
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice; // Обновляем максимальную прибыль
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Максимальная прибыль: " + maxProfit(prices));
+    }
+}
