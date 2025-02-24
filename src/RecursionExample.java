0484946f73c43e629eb74eef740a9a32f48a0af1
dianazavr1 <2235.10002@manas.edu.kg>
2025-02-24 21:23:11 +0600

    Рекурсия

---
+++ src/RecursionExample.java
@@ -0,0 +1,14 @@
+public class RecursionExample {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = sumRecursive(n);
+        System.out.println("Сумма чисел от 1 до " + n + " = " + sum);
+    }
+
+    public static int sumRecursive(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sumRecursive(n - 1);
+    }
+}
