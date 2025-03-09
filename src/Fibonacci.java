c724f5abf416ffbf299ad8d00f1692f0f0941be3
dianazavr1 <2235.10002@manas.edu.kg>
2025-03-09 21:35:17 +0600

    Fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    public static int fibonacciRecursive(int n) {
+        if (n <= 1) return n;
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println("Fibonacci: " + fibonacci(n));
+        System.out.println("Fibonacci (Recursive): " + fibonacciRecursive(n));
+    }
+}
